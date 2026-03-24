656040a70a13f6f82b10e1d9df325810eb5c288b
D3vi7x7 <prasaddevi905@gmail.com>
2026-03-24 20:04:11 +0530

    minor changes

--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -44,7 +44,7 @@ function SolarSystemGLTF() {
   return (
     <>
       <Stars radius={150} depth={60} count={6000} factor={4} saturation={0} fade speed={0.5} />
-      <group ref={group} scale={0.012} position={[0, 1.0, 0]}>
+      <group ref={group} scale={0.012} position={[0, -0.2, 0]}>
         <primitive object={scene} />
       </group>
     </>
